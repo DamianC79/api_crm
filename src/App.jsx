06781b45e19010600f108437fab79c9dbafebe9c
DamianC79 <damianeldiez@gmail.com>
2022-06-10 11:05:12 -0300

    añadiendo variables de entorno

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import EditarCliente from './paginas/EditarCliente'
 import VerCliente from './paginas/VerCliente'
 
 function App() {
+
+  console.log(import.meta.env)
+
+
   return (
   
     <BrowserRouter>
